a6774207279fa733f780d21a40a7283ac856265f
Γεώργιος Παπαδεράκης <gpap@iid.dc>
2024-12-03 11:07:23 +0200

    fixed build errors

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import Home from "./Components/Home"
 import {
   BrowserRouter as Router,
   Route,
-  Link,
-  Routes,
-  BrowserRouter
+  Routes
 } from "react-router-dom";
 
 function App() {
@@ -14,11 +12,11 @@ function App() {
 
 
 
-<BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
 
 
   );
